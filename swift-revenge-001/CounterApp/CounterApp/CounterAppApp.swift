e085ed9830fcf925261b530b8ca7054c54e53528
マッサン <eiji2314@mac.com>
2025-04-21 23:28:11 +0900

    Add files via upload

---
+++ swift-revenge-001/CounterApp/CounterApp/CounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppApp.swift
+//  CounterApp
+//
+//  Created by Eiji Matsushima on 2025/04/21.
+//
+
+import SwiftUI
+
+@main
+struct CounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift-revenge-001/CounterApp/CounterAppTests/CounterAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppTests.swift
+//  CounterAppTests
+//
+//  Created by Eiji Matsushima on 2025/04/21.
+//
+
+import Testing
+@testable import CounterApp
+
+struct CounterAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
